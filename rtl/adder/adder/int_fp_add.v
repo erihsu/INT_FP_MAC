0ea7f503805d31b1263402779fffb24a6f2819df
xuzhenyu69 <zhenyuxu163@163.com>
2020-05-24 17:36:03 +0800

    fix bug in fp16 add

--- rtl/adder/adder/int_fp_add.v
+++ rtl/adder/adder/int_fp_add.v
@@ -1,24 +1,27 @@
-module int_fp_add (mode,clk,rst,a,b,c);
+// This is a piplined adder that cost 2 cycle for fp16 add and 1 cycle for int8 add
 
-    input mode,clk,rst;
+module int_fp_add (mode,clk,rst_n,a,b,c);
+
+    input mode,clk,rst_n;
     input [15:0] a,b;
     output [15:0] c;
 
-    wire a_sign, b_sign, c_sign;
+    wire [10:0] adder_input_1,adder_input_2,aligned_small,adder_output;
+    wire if_sub,a_sign, b_sign, c_sign,c1;
+    wire [4:0] exp_step2;
+    wire [15:0] normalized_out;
+    wire [9:0] normalizer_input;
 
+    reg [14:0] bigger, smaller;
+    reg [10:0] adder_input_1_reg,adder_input_2_reg;
+    reg [9:0] adder_output_reg;
+    reg [15:0] result;
     reg a_larger_b;
 
-    wire [10:0] adder_input_1,adder_input_2,aligned_small,adder_output;
-    wire c1;
-    wire if_sub;
-
-    wire [15:0] normalized_out;
-
-    wire [14:0] bigger, smaller;
-    wire [10:0] normalizer_input;
-    reg [14:0] bigger_reg,smaller_reg;
-    reg [10:0] aligned_small_reg,adder_input_1_reg,adder_input_2_reg,adder_output_reg;
-    reg [15:0] result;
+    // for shifter
+    reg c_sign_tmp1,c_sign_tmp2;
+    reg [4:0] exp_tmp1,exp_tmp2;
+    reg if_sub_tmp1,if_sub_tmp2;
 
     assign a_sign = a[15];
     assign b_sign = b[15];
@@ -27,80 +30,72 @@ module int_fp_add (mode,clk,rst,a,b,c);
     assign c_sign = a_larger_b ? a_sign : b_sign;
 
 
-    assign bigger = bigger_reg;
-    assign smaller = smaller_reg;
+    assign c_sign_step2 = c_sign_tmp2;
+    assign exp_step2 = exp_tmp2;
+    assign if_sub_step2 = if_sub_tmp2;
+
     assign adder_input_1 = adder_input_1_reg;
     assign adder_input_2 = adder_input_2_reg;
     assign normalizer_input = adder_output_reg;
     assign c = result;
 
-// compare two number regardless sign
-always @(posedge clk) begin
-    if (!rst) begin
-        bigger_reg <= 0;
-        smaller_reg <= 0;
-        a_larger_b <= 0;
-    end else if (mode) begin 
+    //compare two number regardless sign
+    always @(*) begin
         if (a[14:0] > b[14:0]) begin
-            bigger_reg = a;
-            smaller_reg = b;
+            bigger = a;
+            smaller = b;
             a_larger_b = 1'b1;
         end else begin 
-            bigger_reg = b;
-            smaller_reg = a;
+            bigger = b;
+            smaller = a;
             a_larger_b = 1'b0;
         end 
     end
-end
 
-always @(posedge clk) begin
-    if(!rst) begin 
-        aligned_small_reg <= 0;
-    end else if(mode) begin 
-        aligned_small_reg <= aligned_small;
-    end 
-end
-
-always @(posedge clk) begin
-    if(!rst) begin 
-        adder_input_1_reg <= 0;
-        adder_input_2_reg <= 0;
-    end else if (mode) begin 
-        adder_input_1_reg <= {1'b1,bigger[9:0]};
-        adder_input_2_reg <= if_sub ? ~aligned_small_reg + 1'b1 : aligned_small_reg;
-    end else begin
-        adder_input_1_reg <= a[7:0];
-        adder_input_2_reg <= b[7:0];
+    //shifter
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin 
+            c_sign_tmp1 <= 0;
+            c_sign_tmp2 <= 0;
+            if_sub_tmp1 <= 0;
+            if_sub_tmp2 <= 0;
+            exp_tmp1 <= 0;
+            exp_tmp2 <= 0;
+        end else begin 
+            c_sign_tmp1 <= c_sign;
+            c_sign_tmp2 <= c_sign_tmp1;
+            exp_tmp1 <= bigger[14:10];
+            exp_tmp2 <= c1 ? exp_tmp1+1'b1 : exp_tmp1; //consider carry
+            if_sub_tmp1 <= if_sub;
+            if_sub_tmp2 <= if_sub_tmp1;
+        end 
     end
-end
 
-always @(posedge clk) begin
-    if(!rst) begin 
-        adder_output_reg <= 0;
-    end else if (mode) begin 
-        adder_output_reg <= adder_output;
-    end else begin 
-        adder_output_reg <= adder_output[7:0];
+    //propose adder and result register
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin 
+            adder_input_1_reg <= 0;
+            adder_input_2_reg <= 0;
+            adder_output_reg <= 0;
+
+        end else if (mode) begin 
+
+            adder_input_1_reg <= {1'b1,bigger[9:0]};
+            adder_input_2_reg <= if_sub ? ~aligned_small + 1'b1 : aligned_small;
+            adder_output_reg <= c1 ? adder_output[10:1] : adder_output[9:0];
+            result <= normalized_out;
+        end else begin
+            adder_input_1_reg <= a[7:0];
+            adder_input_2_reg <= b[7:0];
+            result <= adder_output[7:0];
+        end
     end
-end
 
-always @(posedge clk) begin
-    if(!rst) begin 
-        result <= 0;
-    end else if (mode) begin 
-        result <= normalized_out;
-    end else begin
-        result <= adder_output;
-    end
-end
+    // align small number
+    alignment u1(bigger,smaller,aligned_small);
 
-// align small number
-alignment u1(bigger,smaller,aligned_small);
+    cla_nbit #(.n(11)) u2(adder_input_1,adder_input_2,1'b0,adder_output,c1);
 
-cla_nbit #(.n(11)) u2(adder_input_1,adder_input_2,1'b0,adder_output,c1);
-
-add_normalizer u3(c_sign,bigger[14:10],normalizer_input,normalized_out,if_sub);
-
-//
+    add_normalizer u3(c_sign_step2,exp_step2,normalizer_input,normalized_out,if_sub_step2);
 
 endmodule
--- rtl/multiplier/mul/int_fp_mul.v
+++ rtl/multiplier/mul/int_fp_mul.v
@@ -1,6 +1,8 @@
-module int_fp_mul (mode,clk,rst,a,b,c);
+// This is a piplined multiplier that cost 1 cycle for floating point multiplier and 1 cycle for int8 multiplier
 
-    input mode,clk,rst;
+module int_fp_mul (mode,clk,rst_n,a,b,c);
+
+    input mode,clk,rst_n;
     input [15:0] a,b;
     output [15:0] c;
 
@@ -16,44 +18,42 @@ module int_fp_mul (mode,clk,rst,a,b,c);
     wire [14:0] normalized_out;
     wire [21:0] mantissa_prod;
 
-
     assign a_zero = ~(|a);
     assign b_zero = ~(|b);
     assign c_sign = a[15] ^ b[15];
     assign multiplier_input1 = multiplier_input1_reg;
     assign multiplier_input2 = multiplier_input2_reg;
+    assign mantissa_prod = multiplier_output[21:0];
+    assign c = result;
 
     mul16x16 u1(multiplier_input1,multiplier_input2,multiplier_output);
-    assign mantissa_prod = multiplier_output[21:0] ;
+    
     cla_nbit #(.n(5)) u2(a[14:10],b[14:10],1'b0,sum_exponent,c1); // add exponent
     cla_nbit #(.n(5)) u3(sum_exponent, 5'b10001,1'b0,biased_sum_exponent,c2); // minus bias
     mul_normalizer u4(biased_sum_exponent,mantissa_prod,normalized_out);
 
-always @(posedge clk) begin
-    if(!rst) begin 
-        multiplier_input1_reg <= 0;
-        multiplier_input2_reg <= 0;
-    end else if(mode) begin 
-        multiplier_input1_reg <= {5'b0,1'b1,a[9:0]};
-        multiplier_input2_reg <= {5'b0,1'b1,b[9:0]};
-    end else begin 
-        multiplier_input1_reg <= {8'b0,a[7:0]};
-        multiplier_input2_reg <= {8'b0,b[7:0]};
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin 
+            multiplier_input1_reg <= 0;
+            multiplier_input2_reg <= 0;
+        end else if(mode) begin 
+            multiplier_input1_reg <= {5'b0,1'b1,a[9:0]};
+            multiplier_input2_reg <= {5'b0,1'b1,b[9:0]};
+        end else begin 
+            multiplier_input1_reg <= {8'b0,a[7:0]};
+            multiplier_input2_reg <= {8'b0,b[7:0]};
+        end
+
     end
 
-end
-
-always @(posedge clk) begin
-    if(!rst) begin 
-        result <= 0;
-    end else if (mode) begin 
-        result <= (a_zero | b_zero) ? 16'b0 : {c_sign,normalized_out};
-    end else begin
-        result <= multiplier_output[15:0];
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin 
+            result <= 0;
+        end else if (mode) begin 
+            result <= (a_zero | b_zero) ? 16'b0 : {c_sign,normalized_out};
+        end else begin
+            result <= multiplier_output[15:0];
+        end
     end
-end
-
-
-assign c = result;
 
 endmodule
