d2a7bbdcc93c6eb72f757ae84f8a21665832a081
Eric <31060229+xuzhenyu69@users.noreply.github.com>
2020-07-14 09:50:52 +0800

    revert to single agent topology

--- tb/uvm/macro_def.sv
+++ tb/uvm/macro_def.sv
@@ -1,4 +1,3 @@
 `define factory_create_o(_type,_name) _name = _type::type_id::create(`"_name`")
 `define factory_create_c(_type,_name,_p=this) _name = _type::type_id::create(`"_name`",_p)
 
-// `endif MACRO_DEF_SVH
